63fbcf0197f95cc0c2c29f3e7d51f9d300114b55
Teros <diogo.mm.monteiro@gmail.com>
2025-02-16 17:07:47 +0000

    Update CoordEsf
    Adição de descrição à função

--- MCEE_FichaLaboratorial_03/CoordEsf.m
+++ MCEE_FichaLaboratorial_03/CoordEsf.m
@@ -1,5 +1,10 @@
 function [r, t, f] = CoordEsf(x, y, z)
-% Verificar se os inputs são vetores
+% CoordEsf converte valores em coordenadas cartesianas em coordenadas esféricas.
+% [r, theta, phi] = CoordEsf(x, y, z)
+% x, y, e z devem ser vetores com a mesma dimensão
+% theta e phi são calculados em graus.
+
+% Verificar se são vetores
 if ~isvector(x) || ~isvector(y) || ~isvector(z) 
     error('Os inputs x, y e z devem ser vetores.');
 end
@@ -9,7 +14,7 @@ if ~(numel(x)==numel(y) && numel(y)==numel(z))
     error('Os vetores têm de ter o mesmo número de elementos');
 end
 
-% Expressões
+% Expressões preparadas para calc. matricial
 r = sqrt(x.^2+y.^2+z.^2);
 t = acos(z./r); % theta em radianos
 f = atan(y./x); % phi em radianos
